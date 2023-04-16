6d3144cb9b83c570a0fc9638de25007a2aed063e
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-04-17 01:15:29 +0530

    adding side navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,43 @@
-import Kanban from "../components/Kanban/Kanban"
-import pp from "../components/profile/images/pp.png";
+import { useState } from "react";
+import Kanban from "../components/Kanban/Kanban";
+
+const category = ["Kanban", "Calender", "Discussion Forum"];
 
 const Home = () => {
+  const [currCategory, setCurrCategory] = useState(0);
   return (
     <div>
-        <h1 className="text-xl">
-          <img src={pp} alt="" className="w-[98%] border border-zinc-700 rounded-lg my-4 mx-auto" />
-          <Kanban/>
-        </h1>
-    </div>
-  )
-}
+      <div className="mt-4 flex">
+        <div className="left-0 h-full sticky top-24 overflow-hidden flex flex-col items-start pt-4 gap-4 w-1/6">
+          {category.map((item, index) => (
+            <div
+              key={index}
+              className={`sm:text-xl px-4 py-2 cursor-pointer ${
+                currCategory !== index && "text-zinc-400"
+              } ${
+                currCategory === index &&
+                "text-blue-600 bg-[#0b5be552] w-full border-l-2 border-blue-600"
+              }`}
+              onClick={() => setCurrCategory(index)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
 
-export default Home
+        <div className="mt-20">
+          {currCategory === 0 && <Kanban />}
+          {currCategory === 1 && <Kanban />}
+          {currCategory === 2 && <Kanban />}
+        </div>
+      </div>
+      {/* <img
+        src={pp}
+        alt=""
+        className="w-[98%] border border-zinc-700 rounded-lg my-4 mx-auto"
+      /> */}
+    </div>
+  );
+};
+
+export default Home;
